d7e61845165692849d3204472ce83f129d65a31e
Your Name <name@example.com>
2017-01-17 21:19:08 +0700

    ORM JR 2 Done

--- models/cohort.js
+++ models/cohort.js
@@ -1,10 +1,126 @@
 "use strict"
+class Cohorts {
+    constructor(name, id) {
+        this.name = name
+        this.id = id;
+    }
+    static create(connection, input) {
+        connection.serialize(function() {
+            let query = `INSERT INTO cohorts(name) VALUES ('${input.name}')`;
+            connection.run(query, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("INSERT_DATA SUCCESS");
+                }
+            })
+        })
+        return true;
+    }
 
-import Student from "./student.js";
+    static update(connection, input) {
+        connection.serialize(function() {
+            let query = `UPDATE cohorts SET name='${input.name}' where id='${input.id}'`;
+            connection.run(query, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("UPDATE DATA SUCCESS");
+                }
+            })
+        })
+        return true;
+    }
 
-class Cohort {
+    static delete(connection, id) {
+        connection.serialize(function() {
+            let query = `DELETE from cohorts where id='${id}'`;
+            connection.run(query, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("DELETE DATA SUCCESS");
+                }
+            })
+        })
+        return true;
+    }
+
+    static findById(connection, id) {
+        connection.serialize(function() {
+            let query = `SELECT * from cohorts where id='${id}'`;
+            connection.all(query, function(err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(data);
+                }
+            })
+        })
+        return true;
+    }
+
+    static findAll(connection, params) {
+        let search = function(err, data) {
+            if (!err) {
+                for (var i = params.offset; i <= params.limit; i++) {
+                    console.log(data[i])
+                }
+            } else {
+                console.log("Error");
+            }
+        }
+        connection.serialize(function() {
+            let query = `SELECT * from cohorts`;
+            connection.all(query, search)
+        })
+        return true;
+    }
+
+    static findOrCreate(connection, input) {
+        connection.serialize(function() {
+            let query = `SELECT * from cohorts where name='${input.name}'`;
+            connection.all(query, function(err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (data.length > 0) {
+                        console.log(data);
+                    } else {
+                        let queryInsert = `INSERT INTO cohorts(name) VALUES ('${input.name}')`;
+                        connection.run(queryInsert, function(err) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log("INSERT_DATA SUCCESS");
+                            }
+                        })
+                    }
+                }
+            })
+        })
+        return true;
+    }
+
+    where(connection,field){
+      var search = function(err, data) {
+          if (!err) {
+              for (var i = 0; i < data.length; i++) {
+                  console.log(data[i])
+              }
+          } else {
+              console.log("Error");
+          }
+      }
+      connection.serialize(function() {
+          let query = `SELECT * from cohorts where ${field}`;
+          connection.all(query, search)
+      })
+      return true;
+
+    }
 
 
 }
 
-export default Cohort
+export default Cohorts
--- test/simple.js
+++ test/simple.js
@@ -1,4 +1,29 @@
+"use strict"
 import DBModel from "../models/db_model.js";
 import Student from "../models/student.js";
+var db = new DBModel("../db/test.db")
 
-var db = new DBModel("./db/test.db")
+
+
+function testStudentCreate() {
+  var tambahData = {
+      firstname : "Desdus",
+      lastname : "Kuskus",
+      cohort_id : 1
+  }
+  Student.create(db.connection, new Student(tambahData.firstname, tambahData.lastname, tambahData.cohort_id))
+  cek(tambahData)
+}
+
+function cek(tambahData) {
+    var query = `SELECT * FROM students WHERE firstname='${tambahData.firstname}' AND lastname='${tambahData.lastname}' AND cohort_id='${tambahData.cohort_id}'`
+    db.connection.all(query, function(err) {
+        if (!err && Student.length > 0) {
+            console.log("Test Students Create Sukses");
+        } else {
+            console.log("TEst Stduents Create Gagal");
+        }
+    })
+}
+
+testStudentCreate()
