fb5de10def9a5d4a4f4bd7953e90181bf4149f67
qblol <iqbal.0809@gmail.com>
2017-01-17 21:53:59 +0700

    done

--- models/db_model.js
+++ models/db_model.js
@@ -1,7 +1,34 @@
 "use strict"
+const sqlite = require('sqlite3').verbose();
+let CREATE_TABLE_STUDENTS = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, cohort_id REFERENCES cohorts(id))";
+let CREATE_TABLE_COHORTS = "CREATE TABLE IF NOT EXISTS cohorts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)";
 
 class DBModel {
+  constructor(file){
+    this.connection = new sqlite.Database(file);
+  }
 
+  setup() {
+    let db = this.connection;
+    db.serialize(function() {
+      db.run(CREATE_TABLE_STUDENTS, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('CREATE TABLE STUDENTS');
+        }
+      })
+    })
+    db.serialize(function() {
+      db.run(CREATE_TABLE_COHORTS, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('CREATE TABLE COHORTS');
+        }
+      })
+    })
+  }
 
 }
 
--- test/simple.js
+++ test/simple.js
@@ -2,3 +2,43 @@ import DBModel from "../models/db_model.js";
 import Student from "../models/student.js";
 
 var db = new DBModel("./db/test.db")
+
+function testStudentCreate() {
+  var mockup = {
+    firstname: 'Adam2',
+    lastname: 'Levine',
+    cohort_id: 1
+  }
+
+  Student.create(db.connection, new Student(mockup.firstname, mockup.lastname, mockup.cohort_id))
+  cek(mockup)
+}
+
+function testStudentUpdate() {
+  var mockup = {
+    firstname: 'Muhammad',
+    lastname: 'Iqbal',
+    cohort_id: 20,
+    id: 7
+  }
+
+  Student.update(db.connection, new Student(mockup.firstname, mockup.lastname, mockup.cohort_id, mockup.id))
+  cek(mockup)
+}
+
+function cek(mockup) {
+  var query = `SELECT * FROM students WHERE firstname = '${mockup.firstname}' AND lastname = '${mockup.lastname}' AND cohort_id = '${mockup.cohort_id}'`
+  db.connection.serialize(function(){
+    db.connection.all(query, function(err, students) {
+      if(!err || students.length > 0) {
+        console.log('test success');
+      } else {
+        console.log('test failed');
+      }
+    })
+  })
+
+}
+
+// testStudentCreate()
+testStudentUpdate()
