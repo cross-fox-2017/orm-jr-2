af2fac969effc1f61a37cad6835bda2756a475cb
Hacktiv8 <hacktiv8-3@Hacktiv8s-Mac-mini.local>
2017-01-21 14:20:07 +0700

    done all

--- index.js
+++ index.js
@@ -3,3 +3,25 @@
 import DBModel from "./models/db_model.js";
 import Cohort from "./models/cohort.js";
 import Student from "./models/student.js";
+
+class Help {
+  static help() {
+    console.log('SHOW ALL STUDENTS/COHORT......: lihat Drive code untuk test case!!');
+    console.log('FIND STUDENT/COHORT BY NAME...: lihat Driver code untuk test case!!');
+    console.log('ADD NEW STUDENT/COHORT........: insert(\'firstname\', \'lastname\', \'telepon\')');
+    console.log('REMOVE STUDENT/COHORT.........: delete(id)  ');
+    console.log('UPDATE STUDENT\'s DETAIL.......: update(\'firstname\', \'lastname\', \'telepon\', \'id\')  ');
+  }
+}
+
+const repl  = require("repl");
+var db      = new DBModel("./db/student.db")
+var argv    = process.argv[2]
+
+if(argv == "playtime"){
+  var replStart  = repl.start('> ')
+  replStart.context.dbModel = db
+  replStart.context.Student = Student
+  replStart.context.Cohort  = Cohort
+  replStart.context.help    = Help.help
+}
--- models/db_model.js
+++ models/db_model.js
@@ -1,8 +1,44 @@
 "use strict"
+const sqlite = require("sqlite3").verbose();
+//SQL STATEMENT
+var CREATE_COHORTS  = "CREATE TABLE IF NOT EXISTS cohorts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, year INTEGER);"
+var CREATE_STUDENTS = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, telepon INTEGER, cohort_id INTEGER, FOREIGN KEY (cohort_id) REFERENCES cohorts (id));"
 
 class DBModel {
+  constructor(file){
+    this.connection = new sqlite.Database(file)
+  }
 
+  setup(){
+    var db = this.connection;
+    //CREATE_STUDENTS
+    // let createTableStudents = () =>{
+      db.serialize(function(){
+        db.run(CREATE_STUDENTS, function(err){
+          if(err){
+            console.log(err);
+          }else{
+            console.log('create students success!!');
+          }
+        });
+      });
+    // }//END CREATE_STUDENTS
 
+    //CREATE_COHORTS
+    // let createTableCohorts = ()=>{
+      db.serialize(function(){
+        db.run(CREATE_COHORTS, function(err){
+          if(err){
+            console.log(err);
+          }else{
+            console.log('create cohorts success!!');
+          }
+        });
+      });
+    // }//END CREATE_COHORTS
+    // createTableStudents()
+    // createTableCohorts()
+  }
 }
 
 export default DBModel
